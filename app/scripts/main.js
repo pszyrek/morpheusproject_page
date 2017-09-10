2f7f6bc40cf0f313aa85a76a218c5b8fcc74f770
unknown <piotrekbujna@gmail.com>
2017-09-10 20:47:18 +0200

    modify js script, style handle section, remove team section, add images to img folder, modify few scss files, add animation to link section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,35 @@
 console.log("dupa");
 
-window.addEventListener('scroll', function () {
-    console.log(this);
+window.addEventListener('scroll', function (item) {
     var scroll = this.pageYOffset;
     if (scroll >= 100) {
         document.getElementById("header").classList.add("bg-col-tertiary");
     } else {
         document.getElementById("header").classList.remove("bg-col-tertiary");
     }
-});
 
+    var i = 0;
+
+    function myLoop() {
+        setTimeout(function () {
+            tabs[i].classList.add("hidden-tab");
+            i++;
+            if (i < tabs.length) {
+                myLoop();
+            }
+        }, 300);
+    }
+
+    var tabs = document.getElementsByClassName("link-section__container__tab");
+
+    if (scroll > 1050) {
+        target = true;
+        myLoop();
+    } else {
+        for (var i; i < tabs.length; i++) {
+            tabs[i].classList.remove("hidden-tab");
+        }
+    }
+
+    console.log(scroll);
+});
