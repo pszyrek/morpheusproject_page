3e8df4d418d94594f3e9cb5315da787dd5b54f0d
unknown <piotrekbujna@gmail.com>
2017-09-11 19:48:23 +0200

    add reveal scroll framework, customize handle section, and header, add futures to header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,22 @@
 window.addEventListener('scroll', function (item) {
     var scroll = this.pageYOffset;
+    var navItems = document.getElementsByClassName('header__navbar-item');
+
     if (scroll >= 100) {
         document.getElementById("header").classList.add("bg-col-tertiary");
+        for (var i = 0; i < navItems.length; i++) {
+            navItems[i].classList.add('bg-item');
+        }
     } else {
         document.getElementById("header").classList.remove("bg-col-tertiary");
-    }
-
-    var i = 0;
-
-    function myLoop() {
-        setTimeout(function () {
-            tabs[i].classList.add("hidden-tab");
-            i++;
-            if (i < tabs.length) {
-                myLoop();
-            }
-        }, 300);
-    }
-
-    var tabs = document.getElementsByClassName("link-section__container__tab");
-
-    if (scroll > 1050) {
-        target = true;
-        myLoop();
-    } else {
-        for (var i; i < tabs.length; i++) {
-            tabs[i].classList.remove("hidden-tab");
+        for (var i = 0; i < navItems.length; i++) {
+            navItems[i].classList.remove('bg-item');
         }
     }
 });
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.link-section__container__tab', 300);
 
 var slideIndex = 1;
 showSlides(slideIndex);
