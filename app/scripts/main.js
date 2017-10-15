87a6ca5eb969fac0853cebccd8bba3f012481e1d
pszyrek <pszyrek@MacBook-Pro-Zespo.local>
2017-10-15 14:24:46 +0200

    Dodanie responsywności dla small, large, extra large size za pomocą skryptu i stylów, lekka poprawa styli

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,21 +19,40 @@ $(document).ready(function () {
 window.addEventListener('scroll', function (item) {
     var scroll = this.pageYOffset;
     var navItems = document.getElementsByClassName('header__navbar-item');
+    var header = document.getElementById("header");
+    var brand = document.getElementById("header__brand");
+    var navbar = document.getElementById('header__navbar');
 
-    if (scroll >= 100) {
-        document.getElementById("header").classList.add("bg-header");
+    if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && scroll >= 100) {
+        navbar.style.opacity = '0';
+        header.style.background = 'transparent';
+        brand.style.background = 'rgba(20, 20, 20, 0.84)';
         for (var i = 0; i < navItems.length; i++) {
-            navItems[i].classList.add('bg-item');
+            navItems[i].style.display = 'none';
         }
     } else {
-        document.getElementById("header").classList.remove("bg-header");
+        if (scroll >= 100) {
+            header.style.background = 'rgba(20, 20, 20, 0.84)';
+            for (var i = 0; i < navItems.length; i++) {
+                navItems[i].classList.add('bg-item');
+            }
+        } else {
+            header.style.background = '#151515';
+            for (var i = 0; i < navItems.length; i++) {
+                navItems[i].classList.remove('bg-item');
+            }
+        }
+        navbar.style.opacity = '1';
+        brand.style.background = 'none';
         for (var i = 0; i < navItems.length; i++) {
-            navItems[i].classList.remove('bg-item');
+            navItems[i].style.display = 'flex';
         }
     }
 });
 
-window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({
+    reset: true
+});
 sr.reveal('.link-section__container__tab', 300);
 sr.reveal('.team-section__list-item__tab', 300);
 
@@ -51,8 +70,12 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("step");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
